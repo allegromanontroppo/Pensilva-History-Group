0ed7eb31e910015d137d240b8ba453a47d082350
Mark Holland <rennerholland@googlemail.com>
2020-10-30 18:21:29 +0000

    Use gatsby-background-image for hero

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,20 +1,41 @@
 /* eslint-disable no-unused-vars */
 import * as React from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
+import BackgroundImage from 'gatsby-background-image';
 
 import heroStyles from '../styles/hero.module.scss';
 
 const Hero: React.FC = () => {
-	const { site, placeholderImage } = useStaticQuery(graphql`
+	const {
+		site,
+		mobileHeroImage,
+		tabletHeroImage,
+		desktopHeroImage
+	} = useStaticQuery(graphql`
 		query {
 			site {
 				siteMetadata {
 					title
 				}
 			}
-			placeholderImage: file(relativePath: { eq: "engine-house.jpg" }) {
+
+			mobileHeroImage: file(relativePath: { eq: "engine-house.jpg" }) {
 				childImageSharp {
-					fluid(maxWidth: 1400) {
+					fluid(maxWidth: 500, quality: 90) {
+						...GatsbyImageSharpFluid
+					}
+				}
+			}
+			tabletHeroImage: file(relativePath: { eq: "engine-house.jpg" }) {
+				childImageSharp {
+					fluid(maxWidth: 900, quality: 80) {
+						...GatsbyImageSharpFluid
+					}
+				}
+			}
+			desktopHeroImage: file(relativePath: { eq: "engine-house.jpg" }) {
+				childImageSharp {
+					fluid(maxWidth: 1400, quality: 80) {
 						...GatsbyImageSharpFluid
 					}
 				}
@@ -22,18 +43,17 @@ const Hero: React.FC = () => {
 		}
 	`);
 
+	const sources = [
+		mobileHeroImage.childImageSharp.fluid,
+		{ ...tabletHeroImage.childImageSharp.fluid, media: '(min-width: 501px)' },
+		{ ...desktopHeroImage.childImageSharp.fluid, media: '(min-width: 901px)' }
+	];
+
 	return (
-		<header className={heroStyles.bannerImg}>
-			<div
-				className={heroStyles.atmospheric}
-				style={{
-					backgroundImage: `url(${placeholderImage.childImageSharp.fluid.src})`
-				}}
-			>
-				<div className={heroStyles.titleArea}>
-					<h1>{site.siteMetadata.title}</h1>
-				</div>
-			</div>
+		<header>
+			<BackgroundImage className={heroStyles.atmospheric} fluid={sources}>
+				<h1 className={heroStyles.title}>{site.siteMetadata.title}</h1>
+			</BackgroundImage>
 		</header>
 	);
 };
